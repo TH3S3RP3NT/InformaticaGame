c38b703e218a923462b86bfecb3d7eb98a711c53
TH3S3RP3NT <119533606+TH3S3RP3NT@users.noreply.github.com>
2025-03-31 16:59:09 +0200

    total overhaul

---
+++ src/main.js
@@ -0,0 +1,49 @@
+var muziek = [];
+var currentTrackIndex = 0;
+var musicData;
+function preload() {
+    musicData = loadJSON("assets/music.json", () => {
+        for (var i = 0; i < musicData.Muziek.length; i++) {
+            muziek.push(loadSound('assets/music/' + musicData.Muziek[i].filename));
+        }
+    });
+    achtergrond = loadImage("assets/basicarenabackground.jpg");
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    textFont("Monospace");
+    textSize(40);
+    textAlign(CENTER, CENTER);
+    muziek[currentTrackIndex].play();
+    muziek[currentTrackIndex].onended(playNextTrack);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    }
+
+function draw() {
+    background(achtergrond);
+    text('Pas de grootte van je browserscherm aan zodat de bal zo vaak mogelijk het doel in het midden raakt.', 0, 0, canvas.width, canvas.height / 2);
+    textAlign(RIGHT, BOTTOM);
+    textSize(20);
+
+}
+
+function keyPressed() {
+    if (keyCode == ENTER) {
+            muziek[currentTrackIndex].play();
+    }
+    if (keyCode == N) {
+        playNextTrack()
+    }
+}
+
+function playNextTrack() {
+    currentTrackIndex++;
+    if (currentTrackIndex >= muziek.length) {
+        currentTrackIndex = 0; // Loop back to the first track
+    }
+    muziek[currentTrackIndex].play();
+}
